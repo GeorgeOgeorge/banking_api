0e1fff8f70f114808687e65e2f7e257d9f3e667d
GeorgeOgeorge <george1bsilva@outlook.com>
2025-04-15 15:32:24 -0300

    refactor: renames function

--- banking/api/urls.py
+++ banking/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 from banking.api.views import (
-    create_loan_request,
+    create_loan_route,
     list_loans_route
 )
 
 urlpatterns = [
-    path('loans/create', create_loan_request, name='create_loans'),
+    path('loans/create', create_loan_route, name='create_loans'),
     path('loans/', list_loans_route, name='list_loans'),
 ]
