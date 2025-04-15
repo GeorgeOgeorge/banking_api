e19627fe638d11370c40f917aacefbe0bfc4684c
GeorgeOgeorge <george1bsilva@outlook.com>
2025-04-15 15:31:58 -0300

    feat: register list_loans_route

--- banking/api/urls.py
+++ banking/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from banking.api.views import create_loan_request
+from banking.api.views import (
+    create_loan_request,
+    list_loans_route
+)
 
 urlpatterns = [
-    path('loans/', create_loan_request, name='create_loan'),
+    path('loans/create', create_loan_request, name='create_loans'),
+    path('loans/', list_loans_route, name='list_loans'),
 ]
