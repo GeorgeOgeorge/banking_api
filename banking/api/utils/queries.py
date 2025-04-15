7b3eeec2c8718a8e7fae813ac51cb03e58f3c7c2
GeorgeOgeorge <george1bsilva@outlook.com>
2025-04-15 15:15:04 -0300

    feat: create list loans query

--- banking/api/utils/queries.py
+++ banking/api/utils/queries.py
@@ -12,3 +12,18 @@ CREATE_LOAN_QUERY = """
     )
     RETURNING id, client_id, amount, interest_rate, bank, client_name, ip_address, request_date
 """
+
+LIST_LOAN_QUERY = """
+    select
+        al.id,
+        al.amount,
+        al.interest_rate,
+        al.request_date,
+        al.bank
+    from
+        api_loan al
+    join auth_user au on al.client_id = au.id
+    where au.id = %(client_id)s
+    order by request_date desc
+    limit %(limit)s offset %(offset)s;
+"""
