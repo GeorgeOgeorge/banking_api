d0d3958547e83a0289a93b148fc3b800fce5c564
GeorgeOgeorge <george1bsilva@outlook.com>
2025-04-15 15:18:26 -0300

    feat: creates list loans serializers

--- banking/api/utils/serializers.py
+++ banking/api/utils/serializers.py
@@ -2,21 +2,46 @@ from decimal import Decimal
 from typing import Annotated
 
 from pydantic import BaseModel, Field
-from rest_framework import serializers
+from rest_framework.serializers import (
+    CharField,
+    DateTimeField,
+    DecimalField,
+    IntegerField,
+    IPAddressField,
+    PrimaryKeyRelatedField,
+    Serializer,
+    UUIDField
+)
 
 
-class PaymentResponseSerializer(serializers.Serializer):
-    id = serializers.UUIDField()
-    loan = serializers.UUIDField()
-    payment_date = serializers.DateTimeField()
-    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
+# generics
+class PaginationQueryParams(BaseModel):
+    page: Annotated[int, Field(ge=1, description="Número da página")] = 1
+    limit: Annotated[int, Field(ge=1, description="Quantidade de itens por página")] = 10
+
+    @property
+    def offset(self) -> int:
+        return (self.page - 1) * self.limit
 
 
-class CreateLoanRequestSerializer(serializers.Serializer):
-    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
-    interest_rate = serializers.DecimalField(max_digits=5, decimal_places=2)
-    bank = serializers.CharField(max_length=255)
-    client_name = serializers.CharField(max_length=255)
+class PaginationQueryParamsSerializer(Serializer):
+    page = IntegerField(min_value=1, default=1, help_text="Page number")
+    limit = IntegerField(min_value=1, default=10, help_text="Items per page")
+
+
+# create_loan_request
+class PaymentResponseSerializer(Serializer):
+    id = UUIDField()
+    loan = UUIDField()
+    payment_date = DateTimeField()
+    amount = DecimalField(max_digits=10, decimal_places=2)
+
+
+class CreateLoanRequestSerializer(Serializer):
+    amount = DecimalField(max_digits=10, decimal_places=2)
+    interest_rate = DecimalField(max_digits=5, decimal_places=2)
+    bank = CharField(max_length=255)
+    client_name = CharField(max_length=255)
 
 
 class CreateLoanRequestModel(BaseModel):
@@ -30,14 +55,28 @@ class CreateLoanRequestModel(BaseModel):
         "extra": "forbid"
     }
 
-class CreateLoanResponseSerializer(serializers.Serializer):
-    id = serializers.UUIDField()
-    client = serializers.PrimaryKeyRelatedField(read_only=True)
-    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
-    interest_rate = serializers.DecimalField(max_digits=5, decimal_places=2)
-    ip_address = serializers.IPAddressField()
-    request_date = serializers.DateTimeField()
-    bank = serializers.CharField()
-    client_name = serializers.CharField()
+class CreateLoanResponseSerializer(Serializer):
+    id = UUIDField()
+    client = PrimaryKeyRelatedField(read_only=True)
+    amount = DecimalField(max_digits=10, decimal_places=2)
+    interest_rate = DecimalField(max_digits=5, decimal_places=2)
+    ip_address = IPAddressField()
+    request_date = DateTimeField()
+    bank = CharField()
+    client_name = CharField()
     payments = PaymentResponseSerializer(many=True)
-    remaining_balance = serializers.DecimalField(max_digits=10, decimal_places=2)
+    remaining_balance = DecimalField(max_digits=10, decimal_places=2)
+
+# list_loans_route
+class ListLoansQueryParamsSerializer(PaginationQueryParamsSerializer):
+    ...
+
+class ListLoansQueryParams(PaginationQueryParams):
+    ...
+
+class ListLoansResponse(Serializer):
+    id = UUIDField()
+    amount = DecimalField(max_digits=10, decimal_places=2)
+    interest_rate = DecimalField(max_digits=5, decimal_places=2)
+    bank = CharField()
+    request_date = DateTimeField()
